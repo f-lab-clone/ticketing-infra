569257d36c8cdc8cdcd8b2357d9a4cffa880a332
Junha <junha.ahn.dev@gmail.com>
2023-08-06 08:05:05 +0000

    chore: install kubectl, eksctl

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -13,11 +13,26 @@ unzip awscliv2.zip
 sudo ./aws/install
 rm awscliv2.zip
 
+# aws configure
+# cat ~/.aws/credentials
+# aws sts get-caller-identity
+
 # https://developer.hashicorp.com/terraform/downloads
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
 
-# aws configure
-# cat ~/.aws/credentials
-# aws sts get-caller-identity
+# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm ./kubectl
+rm ./kubectl.sha256
+
+# https://eksctl.io/introduction/#for-unix
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
