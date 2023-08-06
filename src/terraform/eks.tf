665efd64f6a48a2aa87bc8d423e729a2e9681e4f
Hayoung Lim <59460178+hihahayoung@users.noreply.github.com>
2023-08-06 09:22:34 +0000

    feat: set up eks

---
+++ src/terraform/eks.tf
@@ -0,0 +1,73 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+
+  cluster_name    = "main-eks"
+  cluster_version = "1.23"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = concat(module.vpc.public_subnets, module.vpc.private_subnets)
+
+  enable_irsa = true
+
+  eks_managed_node_group_defaults = {
+    ami_type               = "AL2_x86_64"
+    disk_size              = 8
+  }
+
+  eks_managed_node_groups = {
+    backend = {
+      desired_size = 2
+      min_size     = 1
+      max_size     = 2
+
+      labels = {
+        role = "backend"
+      }
+
+      instance_types = ["t2.small"]
+      capacity_type  = "SPOT"
+
+      subnet_ids = module.vpc.public_subnets
+    }
+  }
+
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = module.eks_admins_iam_role.iam_role_arn
+      username = module.eks_admins_iam_role.iam_role_name
+      groups   = ["system:masters"]
+    },
+  ]
+
+  tags = {
+    Environment = "development"
+  }
+}
+
+resource "aws_eks_cluster" "default" {
+  name = "main-cluster"
+  
+  role_arn = module.eks_admins_iam_role.iam_role_arn
+  
+  vpc_config {
+    subnet_ids = concat(module.vpc.public_subnets, module.vpc.private_subnets)
+  }
+
+
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.default.certificate_authority[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.default.id]
+    command     = "aws"
+  }
+}
--- src/terraform/vpc.tf
+++ src/terraform/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  
+
   name = "main"
   cidr = "10.0.0.0/16"
 
