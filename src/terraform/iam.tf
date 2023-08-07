030eadbe42a13a579c55997c935439397efc9de4
Hayoung Lim <59460178+hihahayoung@users.noreply.github.com>
2023-08-07 14:27:26 +0000

    fix: remove unused line

--- src/terraform/iam.tf
+++ src/terraform/iam.tf
@@ -26,10 +26,10 @@ module "eks_admins_iam_role" {
   create_role       = true
   role_requires_mfa = false
 
-  custom_role_policy_arns = [module.allow_eks_access_iam_policy.arn, "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"]
+  custom_role_policy_arns = [module.allow_eks_access_iam_policy.arn]
 
   trusted_role_arns = [
-    "arn:aws:iam::${module.vpc.vpc_owner_id}:root",
+    "arn:aws:iam::${module.vpc.vpc_owner_id}:root"
   ]
 }
 
