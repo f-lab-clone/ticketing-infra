2257e007f279ba6cc4787a728bb36234603b96bc
Hayoung Lim <59460178+hihahayoung@users.noreply.github.com>
2023-08-10 12:45:21 +0900

    Feat/7 aws basic resource (#16)
    * feat: add subnets
    
    * feat: add iam users for eks (admin)
    
    * feat: set up eks
    
    * fix: eks and iam configurations
    
    * chore: install kubectl, eksctl
    
    * chore: remove duplicate config
    
    * fix: some bugs
    
    * fix: aws-auth alerdy exsit bug
    
    * fix: remove unused line
    
    * feat: utilize s3 bucket to store tfstate
    
    * fix: disable private-access to connect to EKS
    
    ---------
    
    Co-authored-by: Junha <junha.ahn.dev@gmail.com>

--- src/terraform/eks.tf
+++ src/terraform/eks.tf
@@ -1,10 +1,10 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
 
-  cluster_name    = "ticketing-eks"
+  cluster_name    = "ticketing-cluster-main"
   cluster_version = "1.27"
 
-  cluster_endpoint_private_access = true
+  cluster_endpoint_private_access = false
   cluster_endpoint_public_access  = true
 
   vpc_id     = module.vpc.vpc_id
@@ -28,7 +28,7 @@ module "eks" {
       }
 
       instance_types = ["t2.small"]
-      capacity_type  = "SPOT"
+      capacity_type  = "ON_DEMAND"
 
       subnet_ids = module.vpc.public_subnets
     }
