361be9d63fb03bc016b4b1cc6a9563a60161e311
Junha <junha.ahn.dev@gmail.com>
2023-08-13 05:59:46 +0000

    fix: remove unused policy

--- src/terraform/ecr.tf
+++ src/terraform/ecr.tf
@@ -3,9 +3,6 @@ module "ecr" {
 
   repository_name = "ticketing-ecr"
 
-  # repository_read_access_arns = [module.allow_ecr_pull_iam_policy.arn]
-  # repository_read_write_access_arns = [module.allow_ecr_push_iam_policy.arn]
-
   repository_lifecycle_policy = jsonencode({
     rules = [
       {
--- src/terraform/iam_ecr_access.tf
+++ src/terraform/iam_ecr_access.tf
@@ -26,27 +26,6 @@ module "allow_ecr_push_iam_policy" {
 
 }
 
-module "allow_ecr_pull_iam_policy" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
-
-  name          = "allow-ecr-pull"
-  create_policy = true
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ecr:BatchGetImage",
-          "ecr:GetDownloadUrlForLayer"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-
 module "ecr_push_iam_group" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
 
@@ -55,26 +34,6 @@ module "ecr_push_iam_group" {
   create_group                      = true
   group_users                       = [module.eks_iam_user_push.iam_user_name]
   custom_group_policy_arns          = [module.allow_ecr_push_iam_policy.arn]
-}
-
-module "ecr_pull_iam_group" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
-
-  name                              = "ecr-pull"
-  attach_iam_self_management_policy = false
-  create_group                      = true
-  group_users                       = [module.eks_iam_user_pull_only.iam_user_name]
-  custom_group_policy_arns          = [module.allow_ecr_pull_iam_policy.arn]
-}
-
-module "eks_iam_user_pull_only" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
-
-  name                          = "ecr_pull_user"
-  create_iam_access_key         = false
-  create_iam_user_login_profile = false
-
-  force_destroy = true
 }
 
 module "eks_iam_user_push" {
