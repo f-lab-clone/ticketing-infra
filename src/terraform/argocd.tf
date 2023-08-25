9d18e5a30802bfee3370560b8b5c76c85ab7e6af
Junha <junha.ahn.dev@gmail.com>
2023-08-25 16:15:31 +0000

    feat: add argocd applications yaml

--- src/terraform/argocd.tf
+++ src/terraform/argocd.tf
@@ -4,3 +4,9 @@ resource "helm_release" "argocd" {
   repository = "https://argoproj.github.io/argo-helm"
   namespace  = "argocd"
 }
+
+resource "kubernetes_manifest" "application" {
+  depends_on = [helm_release.argocd]
+
+  manifest = yamldecode(file("${path.module}/../argocd/applications.yaml"))
+}
