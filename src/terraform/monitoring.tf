167a34424aff51d48d16667158f013bf05ef04ca
Hayoung Lim <59460178+hihahayoung@users.noreply.github.com>
2023-09-06 11:27:24 +0000

    feat: enable custom dashboard on grafana

--- src/terraform/monitoring.tf
+++ src/terraform/monitoring.tf
@@ -3,4 +3,27 @@ resource "helm_release" "kube-prometheus" {
   name       = "prometheus"
   namespace  = "monitoring"
   repository = "https://prometheus-community.github.io/helm-charts"
+
+  values = [
+    "${file("${path.module}/../grafana/custom_values.yaml")}"
+  ]
+}
+
+resource "kubernetes_config_map" "grafana-dashboards-custom" {
+  metadata {
+    name      = "grafana-dashboard-custom"
+    namespace = kubernetes_namespace.monitoring-ns.metadata[0].name
+
+    labels = {
+      grafana_dashboard = 1
+    }
+
+    annotations = {
+      k8s-sidecar-target-directory = "/tmp/dashboards/custom"
+    }
+  }
+
+  data = {
+    "test-dashboard.json" = file("${path.module}/../grafana/dashboards/test_dashboard.json")
+  }
 }
