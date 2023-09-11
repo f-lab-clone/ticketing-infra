60762e3358d87e5cafcf338d8030f7ccf391f6a3
Junha <junha.ahn.dev@gmail.com>
2023-09-11 14:37:26 +0000

    feat: add secret manager

---
+++ src/terraform/secret-manager.tf
@@ -0,0 +1,42 @@
+module "secrets_manager" {
+  source = "terraform-aws-modules/secrets-manager/aws"
+
+  name_prefix             = "development/ticketing-backend"
+
+
+  ignore_secret_changes = true
+  secret_string = jsonencode({
+    MYSQL_PASSWORD   = ""
+#    PORT: ""
+#    MYSQL_HOST: ""
+#    MYSQL_PORT: ""
+#    MYSQL_USERNAME: ""
+#    MYSQL_SCHEMA: ""
+#    JWT_SECRET: ""
+#    JWT_EXPIRATION_HOURS: ""
+#    JWT_ISSUER: ""
+  })
+}
+
+module "secrets_manager_access_policy" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+
+  name          = "secrets-manager-access-policy"
+  create_policy = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "secretsmanager:GetSecretValue", 
+          "secretsmanager:DescribeSecret"
+        ]
+        Effect   = "Allow"
+        Resource = [
+          module.secrets_manager.secret_arn
+        ]
+      }
+    ]
+  })
+}
