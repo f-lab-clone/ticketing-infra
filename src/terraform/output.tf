a14acbf4bebb9138d1d43efbc82922d86d436747
Junha <junha.ahn.dev@gmail.com>
2023-09-12 10:42:45 +0900

    Feat/4 secret manager (#45)
    * feat: add secret manager
    
    * feat: add service-account for secret-manager
    
    * fix: remove unused output
    
    * feat: install secrets-store-csi-driver
    
    * feat: add tmp user (for connection test)
    
    * feat: update secrets
    
    * chore: add comment

--- src/terraform/output.tf
+++ src/terraform/output.tf
@@ -1,19 +1,6 @@
-output "eks_admins_iam_role_arn" {
-  value = module.eks_admins_iam_role.iam_role_arn	
-}
-
-output "eks_admins_iam_role_name" {
-  value = module.eks_admins_iam_role.iam_role_name	
-}
-
 output "cluster_name" {
   value = module.eks.cluster_name
 }
-
-# output "aws_auth_configmap_yaml" {
-#   value = module.eks.aws_auth_configmap_yaml
-# }
-
 
 output "bastion_public_ip" {
   description = "Public IP address of the bastion host"
---
+++ src/terraform/secret-manager.tf
@@ -0,0 +1,59 @@
+resource "helm_release" "secrets-store-csi-driver" {
+  chart            = "secrets-store-csi-driver"
+  name             = "csi-secrets-store"
+  namespace        = "kube-system"
+  repository       = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
+
+  set {
+    name  = "syncSecret.enabled"
+    value = "true"
+  }
+}
+resource "helm_release" "secrets-store-csi-driver-provider-aws" {
+  chart            = "secrets-store-csi-driver-provider-aws"
+  name             = "secrets-provider-aws"
+  namespace        = "kube-system"
+  repository       = "https://aws.github.io/secrets-store-csi-driver-provider-aws"
+}
+
+module "secrets_manager" {
+  source = "terraform-aws-modules/secrets-manager/aws"
+
+  name = "development/ticketing-backend-secret"
+
+
+  ignore_secret_changes = true
+  secret_string = jsonencode({
+    MYSQL_PASSWORD = ""
+    MYSQL_USERNAME = ""
+    MYSQL_HOST = ""
+    MYSQL_PORT = ""
+    MYSQL_SCHEMA = ""
+    JWT_SECRET = ""
+    JWT_EXPIRATION_HOURS = ""
+    JWT_ISSUER = ""
+  })
+}
+
+module "secrets_manager_access_policy" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+
+  name          = "secrets-manager-access-policy"
+  create_policy = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "secretsmanager:GetSecretValue", 
+          "secretsmanager:DescribeSecret"
+        ]
+        Effect   = "Allow"
+        Resource = [
+          module.secrets_manager.secret_arn
+        ]
+      }
+    ]
+  })
+}
