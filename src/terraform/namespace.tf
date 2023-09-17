d17f55297d7b58022b8c8e99ed35b7d49886e6c6
junha-ahn <junha.ahn.dev@gmail.com>
2023-09-17 11:35:32 +0900

    fix: remove unused feat

--- src/terraform/namespace.tf
+++ src/terraform/namespace.tf
@@ -1,24 +1,15 @@
 resource "kubernetes_namespace" "argo-ns" {
   metadata {
     name = "argocd"
-    annotations = {
-      "scheduler.alpha.kubernetes.io/node-selector": "role=backend"  
-    }
   }
 }
 resource "kubernetes_namespace" "monitoring-ns" {
   metadata {
     name = "monitoring"
-    annotations = {
-      "scheduler.alpha.kubernetes.io/node-selector": "role=backend"  
-    }
   }
 }
 resource "kubernetes_namespace" "ingress-nginx-ns" {
   metadata {
     name = "ingress-nginx"
-    annotations = {
-      "scheduler.alpha.kubernetes.io/node-selector": "role=ingress"  
-    }
   }
 }
