681112464e92d64f37bca8f6d8c1a59ff62658ac
Hayoung Lim <59460178+hihahayoung@users.noreply.github.com>
2023-09-18 14:33:27 +0000

    feat: initialize ebs policy and role

---
+++ src/terraform/ebs.tf
@@ -0,0 +1,46 @@
+module "ebs_csi_eks_role" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name = "ebs_csi"
+
+  # 밑에서 csi driver를 통해 생성한 policy를 attach
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               =  module.eks.oidc_provider_arn
+      namespace_service_accounts = ["default:ticketing-ebs"]
+    }
+  }
+}
+
+resource "helm_release" "ebs_csi_driver" {
+  name       = "aws-ebs-csi-driver"
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+  chart      = "aws-ebs-csi-driver"
+
+  set {
+    name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    type  = "string"
+    value = module.ebs_csi_eks_role.iam_role_arn
+  }
+}
+
+# 새로운 storage class 생성
+resource "kubernetes_storage_class" "gp3-storage" {
+  depends_on = [helm_release.ebs_csi_driver, module.ebs_csi_eks_role]
+  metadata {
+    name = "gp3-storage"
+    # annotations = {
+    #   "storageclass.kubernetes.io/is-default-class" = "true"
+    # }
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  volume_binding_mode = "WaitForFirstConsumer"
+  allow_volume_expansion = true
+  parameters = {
+    "encrypted" = "false"
+    "fsType" = "ext4"
+    "type" = "gp3"
+  }
+}
