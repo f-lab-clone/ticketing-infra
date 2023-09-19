d56415cc1577eeb31f93e5142e61aacf7232815d
junha-ahn <junha.ahn.dev@gmail.com>
2023-09-19 15:26:41 +0900

    fix: ebs

--- src/terraform/ebs.tf
+++ src/terraform/ebs.tf
@@ -1,31 +1,7 @@
-module "ebs_csi_eks_role" {
-  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  role_name = "ebs_csi"
-
-  # 밑에서 csi driver를 통해 생성한 policy를 attach
-  attach_ebs_csi_policy = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               =  module.eks.oidc_provider_arn
-      namespace_service_accounts = ["default:ticketing-ebs"]
-    }
-  }
-}
-
-resource "kubernetes_service_account" "ticketing_ebs" {
-  metadata {
-    name = "ticketing-ebs"
-    namespace = "default"
-    annotations = {
-      "eks.amazonaws.com/role-arn" = module.ebs_csi_eks_role.iam_role_arn
-    }
-  }
-}
 
 resource "helm_release" "ebs_csi_driver" {
   name       = "aws-ebs-csi-driver"
-  namespace  = "default"
+  namespace  = "kube-system"
   repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
   chart      = "aws-ebs-csi-driver"
 
@@ -36,18 +12,34 @@ resource "helm_release" "ebs_csi_driver" {
   }
 }
 
-# 새로운 storage class 생성
+module "ebs_csi_eks_role" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name = "ebs_csi"
+
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               =  module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+
 resource "kubernetes_storage_class" "gp3-storage" {
   depends_on = [helm_release.ebs_csi_driver, module.ebs_csi_eks_role]
   metadata {
-    name = "gp3-storage"
-    # annotations = {
-    #   "storageclass.kubernetes.io/is-default-class" = "true"
-    # }
+    name = "gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
   }
+
   storage_provisioner = "ebs.csi.aws.com"
   volume_binding_mode = "WaitForFirstConsumer"
   allow_volume_expansion = true
+  reclaim_policy = "Delete"
+
   parameters = {
     "encrypted" = "false"
     "fsType" = "ext4"
