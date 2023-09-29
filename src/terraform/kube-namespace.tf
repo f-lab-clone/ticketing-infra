01065b864f18c7dcafc6d58cf949960333e4c5ed
Junha <junha.ahn.dev@gmail.com>
2023-09-30 00:29:56 +0900

    Feat/69 pod resource (#71)
    * feat: customize long_query_time on mysql
    
    * feat: add resource-quota
    
    * feat: add pod resources
    
    * fix: request down
    
    * feat: change ingress node type (t2 => t3)
    
    ---------
    
    Co-authored-by: Hayoung Lim <59460178+hihahayoung@users.noreply.github.com>

--- src/terraform/kube-namespace.tf
+++ src/terraform/kube-namespace.tf
@@ -13,3 +13,16 @@ resource "kubernetes_namespace" "ingress-nginx-ns" {
     name = "ingress-nginx"
   }
 }
+
+
+resource "kubernetes_manifest" "argocd-quata" {
+  depends_on = [kubernetes_namespace.argo-ns]
+
+  manifest = yamldecode(file("${path.module}/../kubernetes/namespace-resources/argocd-quata.yaml"))
+}
+
+resource "kubernetes_manifest" "monitoring-quata" {
+  depends_on = [kubernetes_namespace.monitoring-ns]
+
+  manifest = yamldecode(file("${path.module}/../kubernetes/namespace-resources/monitoring-quata.yaml"))
+}
