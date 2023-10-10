f6e34fecb9193e8faf3462fa5e436150e34abb70
junha-ahn <junha.ahn.dev@gmail.com>
2023-10-11 00:12:59 +0900

    feat: add init sql

--- src/mysql/user.sql
+++ src/mysql/user.sql
@@ -1,5 +1,8 @@
-CREATE USER 'exporter'@'%' IDENTIFIED BY 'pw1234' WITH MAX_USER_CONNECTIONS 3;
+CREATE USER 'exporter'@'%' IDENTIFIED BY 'password-string' WITH MAX_USER_CONNECTIONS 3;
 GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
 
-CREATE USER 'backend'@'%' IDENTIFIED BY 'pw1234';
+CREATE USER 'backend'@'%' IDENTIFIED BY 'password-string';
 GRANT all privileges  ON ticketingdb.* TO 'backend'@'%';
+
+CREATE USER 'k6_init'@'%' IDENTIFIED BY 'password-string';
+GRANT all privileges  ON ticketingdb.* TO 'k6_init'@'%';
