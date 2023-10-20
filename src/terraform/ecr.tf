17a84a8bb5ceeff7b2e25fd5c981b26019e0d8a5
junha-ahn <junha.ahn.dev@gmail.com>
2023-10-20 22:40:06 +0900

    feat: add ecr

--- src/terraform/ecr.tf
+++ src/terraform/ecr.tf
@@ -10,12 +10,43 @@ module "ecr" {
     rules = [
       {
         rulePriority = 1,
-        description  = "Keep last 30 images",
+        description  = "Keep last 10 images",
         selection = {
           tagStatus     = "tagged",
           tagPrefixList = ["v"],
           countType     = "imageCountMoreThan",
-          countNumber   = 30
+          countNumber   = 10
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Environment = "development"
+  }
+}
+
+module "ecr-queuing" {
+  source = "terraform-aws-modules/ecr/aws"
+
+  repository_name = "ticketing-queuing-ecr"
+
+  repository_image_tag_mutability = "MUTABLE"
+  repository_force_delete = true
+
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 10 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 10
         },
         action = {
           type = "expire"
